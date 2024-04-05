89c58ec19ef2153d81c9343374d1b897f4bee942
sjzt_yjh <179740385@qq.com>
2024-04-05 10:35:17 +0800

    chore: update build

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: 'docs'
+  },
+  base: './'
 })
