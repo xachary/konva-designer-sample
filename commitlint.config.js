834c7a270897de8c726ae6cd846ecc4434489bd6
xachary <179740385@qq.com>
2024-05-17 13:21:24 +0800

    chore: add type-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,28 @@
-export default { extends: ['@commitlint/config-conventional'] }
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // <type>枚举
+    'type-enum': [
+      2,
+      'always',
+      [
+        'add', // 添加依赖
+        'delete', // 删除代码/文件
+        'feat', // 增加新功能
+        'fix', // 修复bug
+        'style', // 样式修改不影响逻辑
+        'merge', // 合并分支
+        'perfect', // 功能完善
+        'docs', // 修改文档
+        'refactor', // 代码重构
+        'test', // 单元测试修改
+        'ci', // 持续继承
+        'release', // 发布
+        'deploy', // 部署
+        'chore', // 更改配置文件
+        'revert', // 版本回退
+        'wip' // 正在开发中
+      ]
+    ],
+  }
+}
